52d54d1208b89d81fcc6655a3cf4172808aff2cf
AgohChris <agihchris90@gmail.com>
2025-04-29 22:22:12 +0000

    Auto-commit du : 29-04-2025 à 22:22:12

--- oldtopic/admin.py
+++ oldtopic/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Etudiant)
+admin.site.register(Admin)
+admin.site.register(SuperAdmin)
+admin.site.register(Stats)
+admin.site.register(Suspendre)
+admin.site.register(Gerer)
+admin.site.register(Telecharger)
+admin.site.register(Visualiser)
+admin.site.register(Sujet)
+admin.site.register(Historique)
+
--- oldtopic/urls.py
+++ oldtopic/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('subjects/', SujetListView.as_view(), name='sujet-list'),
-    path('subjects/<int:pk>/', SujetDetailView.as_view(), name='sujet-detail'),
+    # path('subjects/', SujetListView.as_view(), name='sujet-list'),
+    # path('subjects/<int:pk>/', SujetDetailView.as_view(), name='sujet-detail'),
 ]
